9b960ed003b4a1100d0cd35ffb55bf1d7ea39c8f
antonioconte <antonio.conte05@live.it>
2019-07-22 17:19:59 +0200

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,1 @@
-virtualenv -p python3.5 venv && source venv/bin/activate && pip --no-cache-dir install -r requirements.txt  && python3 -m spacy download en_core_web_sm
-&& python app.py
+virtualenv -p python3.5 venv && source venv/bin/activate && pip --no-cache-dir install -r requirements.txt  && python3 -m spacy download en_core_web_sm && echo $(hostname -i) && python app.py
